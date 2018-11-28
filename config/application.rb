f344fcc4fd6509c65ae2c1bea6e5a837c8c75750
JoralmoPro <JoralmoPro@gmail.com>
2018-11-28 18:17:15 -0500

    Cambiando sqlite3 por pg 🕶️

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module FinalArquitectura
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,4 +19,3 @@ module FinalArquitectura
     # the framework and any gems in your application.
   end
 end
-config.assets.initialize_on_precompile = false
